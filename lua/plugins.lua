23b8cfe027a4c41711e304ee130c4fd63d33fa8d
Jonas Kuball <jonas.kuball@justadd.ai>
2023-02-24 15:08:48 +0100

    🎨 adds lua rules to pre-commit (and applies them)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,52 +3,52 @@
 local Module = {}
 
 function Module.setup()
-  -- Indicate first time installation
-  local packer_bootstrap = false
+    -- Indicate first time installation
+    local packer_bootstrap = false
 
-  -- packer.nvim configuration
-  local conf = {
-    display = {
-      open_fn = function()
-        return require("packer.util").float { border = "rounded" }
-      end,
-    },
-  }
+    -- packer.nvim configuration
+    local conf = {
+        display = {
+            open_fn = function()
+                return require("packer.util").float({ border = "rounded" })
+            end,
+        },
+    }
 
-  -- Check if packer.nvim is installed
-  -- Run PackerCompile if there are changes in this file
-  local function packer_init()
-    local fn = vim.fn
-    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-      packer_bootstrap = fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-      }
-      vim.cmd [[packadd packer.nvim]]
+    -- Check if packer.nvim is installed
+    -- Run PackerCompile if there are changes in this file
+    local function packer_init()
+        local fn = vim.fn
+        local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+        if fn.empty(fn.glob(install_path)) > 0 then
+            packer_bootstrap = fn.system({
+                "git",
+                "clone",
+                "--depth",
+                "1",
+                "https://github.com/wbthomason/packer.nvim",
+                install_path,
+            })
+            vim.cmd([[packadd packer.nvim]])
+        end
+        vim.cmd("autocmd BufWritePost plugins.lua source <afile> | PackerCompile")
     end
-    vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
-  end
-  
-  -- Plugins
-  local function plugins(use)
-    use { "wbthomason/packer.nvim" }
 
-    if packer_bootstrap then
-      print "Restart Neovim required after installation!"
-      require("packer").sync()
+    -- Plugins
+    local function plugins(use)
+        use({ "wbthomason/packer.nvim" })
+
+        if packer_bootstrap then
+            print("Restart Neovim required after installation!")
+            require("packer").sync()
+        end
     end
-  end
 
-  packer_init()
+    packer_init()
 
-  local packer = require "packer"
-  packer.init(conf)
-  packer.startup(plugins)
+    local packer = require("packer")
+    packer.init(conf)
+    packer.startup(plugins)
 end
 
 return Module
