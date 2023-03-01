aa92163285254ab35e2db1c7a1cf4b561ea335d1
Jonas Kuball <jonas.kuball@justadd.ai>
2023-03-01 13:38:39 +0100

    🔧 enables 24-bit rgb color in the terminal

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,5 +1,7 @@
-local common_options = { noremap = true, silent = true }
+-- Enables 24-bit RGB color in the terminal
+vim.opt.termguicolors = true
 
 -- Jump between diagnostics marks
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, common_options)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, common_options)
+local common_map_options = { noremap = true, silent = true }
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, common_map_options)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, common_map_options)
