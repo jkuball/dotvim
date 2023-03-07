49c37ffbe1845a8d30f4b5b00227aaa3c218245a
Jonas Kuball <jonas.kuball@justadd.ai>
2023-03-07 10:39:30 +0100

    🔧 sets some default vim options

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -5,3 +5,10 @@ vim.opt.termguicolors = true
 local common_map_options = { noremap = true, silent = true }
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, common_map_options)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, common_map_options)
+
+-- Open splits in a more natural order (for me)
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Only show a completion menu, don't open a preview window.
+vim.opt.completeopt = "menuone"
