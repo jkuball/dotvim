2a5e773bd2d953cec86609faf2348c1f47c38a5b
Jonas Kuball <jonas.kuball@justadd.ai>
2023-03-08 14:01:10 +0100

    🔧 adds mappings for telescope

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,7 +1,17 @@
 local Module = {}
 
 function Module.setup()
-    require("telescope").setup({})
+    require("telescope").setup({
+        defaults = {
+            mappings = {
+                i = {
+                    -- Move around in the matches like I am used to from FZF.
+                    ["<C-j>"] = require("telescope.actions").move_selection_next,
+                    ["<C-k>"] = require("telescope.actions").move_selection_previous,
+                },
+            },
+        },
+    })
 end
 
 return Module
