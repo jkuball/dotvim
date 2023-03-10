7ebd58ed6ceff57e3451964b6fb97c1df3039744
Jonas Kuball <jonas.kuball@justadd.ai>
2023-03-10 12:52:37 +0100

    ✨ reloads files automatically
    when they're changed outside of vim

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -10,9 +10,18 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, common_map_options)
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Only show a completion menu, don't open a preview window.
+-- Only show a completion menu, don't open a preview window
 vim.opt.completeopt = "menuone"
 
 -- Enable undo persistence
 vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
 vim.opt.undofile = true
+
+-- Automatically reload files from disk if there are no local changes
+vim.opt.autoread = true
+vim.opt.updatetime = 300
+vim.api.nvim_create_autocmd({ "FocusGained", "CursorHold" }, {
+    callback = function()
+        vim.cmd.checktime()
+    end,
+})
