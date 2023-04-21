6802b745127bd76256b534e37c8a25c02f0d2acd
Jonas Kuball <jonas.kuball@justadd.ai>
2023-04-21 16:59:23 +0200

    ➕🐛 adds missing cmp-buffer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -41,6 +41,7 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "dcampos/cmp-snippy",
             "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
             "dcampos/nvim-snippy",
             "hrsh7th/cmp-nvim-lsp-signature-help",
         },
