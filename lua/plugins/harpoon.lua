24c6d5ec612e8b1e5f10da9a5f58b8bb1c8f22a1
Jonas Kuball <jonas.kuball@lector.ai>
2023-05-24 22:58:29 +0200

    ✨ begins to configure usage of harpoon terminals

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,10 +5,17 @@
 
 return {
     "ThePrimeagen/harpoon",
-    opts = {},
-    init = function()
+    opts = {
+        global_settings = {
+            enter_on_sendcmd = true,
+        },
+    },
+    config = function(_, opts)
+        require("harpoon").setup(opts)
+
         local harpoon_ui = require("harpoon.ui")
         local harpoon_mark = require("harpoon.mark")
+        local harpoon_term = require("harpoon.term")
         local wk = require("which-key")
 
         local _nav_file = function(num)
@@ -17,9 +24,29 @@ return {
             end
         end
 
+        local _nav_term = function(num)
+            return function()
+                harpoon_term.gotoTerminal(num)
+            end
+        end
+
         local invisible = "which_key_ignore"
 
         wk.register({
+            t = {
+                name = "+term",
+                t = { require("harpoon.cmd-ui").toggle_quick_menu, "harpoon cmd ui" },
+                ["#"] = "Go to Harpoon Terminal (1-9)",
+                ["1"] = { _nav_term(1), invisible },
+                ["2"] = { _nav_term(2), invisible },
+                ["3"] = { _nav_term(3), invisible },
+                ["4"] = { _nav_term(4), invisible },
+                ["5"] = { _nav_term(5), invisible },
+                ["6"] = { _nav_term(6), invisible },
+                ["7"] = { _nav_term(7), invisible },
+                ["8"] = { _nav_term(8), invisible },
+                ["9"] = { _nav_term(9), invisible },
+            },
             h = {
                 name = "+harpoon",
                 h = { harpoon_ui.toggle_quick_menu, "Toggle Quick Menu" },
