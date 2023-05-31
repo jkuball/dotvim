688810f02f2c559ffb1d794d066fa9e64a2799fe
Jonas Kuball <jonas.kuball@justadd.ai>
2023-05-31 09:51:07 +0200

    ✨ adds nvim-blame-line

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,7 +1,18 @@
--- Consider replacing vim-gitgutter with gitsigns.nvim.
+-- Consider replacing vim-gitgutter with gitsigns.nvim (it also provices a blame line).
 -- - https://github.com/lewis6991/gitsigns.nvim
 
 return {
     "tpope/vim-fugitive",
     "airblade/vim-gitgutter",
+    {
+        "tveskag/nvim-blame-line",
+        config = function()
+            require("which-key").register({
+                g = {
+                    name = "+git",
+                    b = { vim.cmd.ToggleBlameLine, "Toggle Blame Line" },
+                },
+            }, { prefix = "<Leader>" })
+        end,
+    },
 }
