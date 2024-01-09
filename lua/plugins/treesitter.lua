129b541765fabf75072cf63f640ba586a79d17a9
Jonas Kuball <jonas.kuball@lector.ai>
2024-01-09 14:25:38 +0100

    🔧 makes treesitter installation async

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,13 +2,13 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = function()
-            require("nvim-treesitter.install").update({ with_sync = true })()
+            require("nvim-treesitter.install").update({ with_sync = false })()
         end,
         opts = {
             -- A list of parser names, or "all"
             ensure_installed = "all",
             -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = true,
+            sync_install = false,
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = true,
