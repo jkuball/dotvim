d494437071172e6ef680d5c481b77b87ed131df9
Jonas Kuball <jonas.kuball@lector.ai>
2024-02-28 11:56:19 +0100

    ⇁ harpoon it up

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,4 +1,7 @@
 local wk = require("which-key")
 
 -- prefixes for global keys setup by lazy
-wk.register({ f = { name = "+find" } }, { prefix = "<Leader>" })
+wk.register({
+	f = { name = "+find" },
+	h = { name = "+harpoon" },
+}, { prefix = "<Leader>" })
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,1 +1,51 @@
-return {}
+
+--- @type LazyPluginSpec[]
+local specs = {{
+	"https://github.com/ThePrimeagen/harpoon/",
+	branch = "harpoon2", -- currently in rewrite, let's test it. NOTE: Terminal support seems to be missing right now, come back to that later.
+	--- @type HarpoonPartialConfig
+	opts = {},
+	keys = {
+		{
+			"<Leader>hh",
+			function()
+				local harpoon = require("harpoon")
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end,
+			desc = "Toggle Harpoon UI"
+		},
+		{
+			"<Leader>ha",
+			function()
+				require("harpoon"):list():append()
+			end,
+			desc = "Toggle Harpoon UI"
+		},
+		{
+			"]h",
+			function()
+				require("harpoon"):list():next({ui_nav_wrap=true})
+			end,
+			desc = "Next Harpooned File (wrapping)"
+		},
+		{
+			"[h",
+			function()
+				require("harpoon"):list():prev({ui_nav_wrap=true})
+			end,
+			desc = "Prev Harpooned File (wrapping)"
+		},
+		{ "<Leader>1", function() require("harpoon"):list():select(1) end, desc = "⇁ #1" },
+		{ "<Leader>2", function() require("harpoon"):list():select(2) end, desc = "⇁ #2" },
+		{ "<Leader>3", function() require("harpoon"):list():select(3) end, desc = "⇁ #3" },
+		{ "<Leader>4", function() require("harpoon"):list():select(4) end, desc = "⇁ #4" },
+		{ "<Leader>5", function() require("harpoon"):list():select(5) end, desc = "⇁ #5" },
+		{ "<Leader>6", function() require("harpoon"):list():select(6) end, desc = "⇁ #6" },
+		{ "<Leader>7", function() require("harpoon"):list():select(7) end, desc = "⇁ #7" },
+		{ "<Leader>8", function() require("harpoon"):list():select(8) end, desc = "⇁ #8" },
+		{ "<Leader>9", function() require("harpoon"):list():select(9) end, desc = "⇁ #9" },
+	},
+	dependencies = { "https://github.com/nvim-lua/plenary.nvim" },
+}}
+
+return specs
